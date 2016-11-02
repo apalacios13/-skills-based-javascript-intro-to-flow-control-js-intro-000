1d11f26e61b03ea182e03e9ceb37694bb0e872a3
maideline <anap27611@students.laalliance.org>
2016-11-02 15:57:04 +0000

    Third saved code from nitrous.

---
+++ index.js
@@ -0,0 +1,37 @@
+// write your code below!
+
+var basicTeenager = function basicTeenager(age) {
+  if(age >=13 && age <=19 ){
+  return "You are a teenager!"
+
+  } else{
+    return undefined
+  }
+}
+
+var teenager = function teenager(age) {
+  if(age >=13 && age <=19 ){
+  return "You are a teenager!"
+
+  } else{
+    return "You are not a teenager"
+}
+}
+var ageChecker = function ageChecker(age) {
+  if (age >=13 && age <=19 ){
+    return "You are a teenager!"
+  } else if (age ===20 ){
+    return "You are a grownup"
+
+  } else {
+return "you are a kid"
+  }
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
